2906a5cbc6e2b1052b5cfe93ea9105cdcad9a1dd
Guy <guydesu@gmail.com>
2014-03-07 21:45:28 +0000

    rename

---
+++ Classes/ABox.h
@@ -0,0 +1,23 @@
+#ifndef __DS_NPC__
+#define __DS_NPC__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class ABox : public cocos2d::Node
+{
+public:
+	ABox();
+	~ABox();
+
+	void visit();
+
+protected:
+	
+
+private:
+
+};
+
+#endif 
